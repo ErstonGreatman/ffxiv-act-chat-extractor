37ad474adf1e555ce942b525df6c6f7ba07a43ed
John Melvin III <john@mapletonhill.net>
2020-07-31 18:44:59 -0600

    - Added parsing for Cross-World names

--- src/components/LogViewer.tsx
+++ src/components/LogViewer.tsx
@@ -5,6 +5,8 @@ import {
   CHANNEL_CODES,
   CHANNELS,
 } from '../constants/Channels';
+import { parseName } from '../LogParser';
+import { RegEx } from '../constants/RegEx';
 
 
 const styles = {
@@ -39,15 +41,17 @@ type Props = {
  */
 const LogViewer: React.FC<Props> = (props: Props) => {
   const renderSender = (code: string, sender: string) => {
+    const parsedSender = parseName(sender);
+
     switch (code) {
       case CHANNEL_CODES.EMOTE:
         return <></>;
       case CHANNEL_CODES.WHISPER:
-        return <span>{sender}> </span>;
+        return <span>{parsedSender}> </span>;
       case CHANNEL_CODES.PARTY:
-        return <span>({sender}) </span>;
+        return <span>({parsedSender}) </span>;
       case CHANNEL_CODES.FREE_COMPANY:
-        return <span>[Free Company]{`<${sender}>`} </span>;
+        return <span>[Free Company]{`<${parsedSender}{>`} </span>;
       case CHANNEL_CODES.LINKSHELL1:
       case CHANNEL_CODES.LINKSHELL2:
       case CHANNEL_CODES.LINKSHELL3:
@@ -57,17 +61,23 @@ const LogViewer: React.FC<Props> = (props: Props) => {
       case CHANNEL_CODES.LINKSHELL7:
       case CHANNEL_CODES.LINKSHELL8:
         const linkshell = parseInt(code) - 9;
-        return <span>[{linkshell}]{`<${sender}>`} </span>;
+        return <span>[{linkshell}]{`<${parsedSender}>`} </span>;
       default:
-        return <span>{sender}: </span>;
+        return <span>{parsedSender}: </span>;
     }
   };
+
+  // Emotes are a special case and need to be name-checked
+  const renderEmote = (message: string) => message.replace(
+    new RegExp(RegEx.name, 'g'),
+    (match) => parseName(match),
+  );
 
   const renderLogEntry = (entry: MatchedLogLine) => (
     <div css={styles.logEntry(entry.code)} key={`${entry.time}-${entry.sender}`}>
       {props.showTimeStamp && <span>[{entry.time}] </span>}
       {renderSender(entry.code, entry.sender)}
-      <span>{entry.message}</span>
+      <span>{entry.code === CHANNEL_CODES.EMOTE ? renderEmote(entry.message) : entry.message}</span>
     </div>
   );
 
---
+++ src/constants/RegEx.ts
@@ -0,0 +1,4 @@
+export const RegEx = {
+  name: /(?<name>[A-Z][a-z]+\s[A-Z][a-z]+)(?<realm>[A-Z][a-z]+)?/,
+  message: /00\|\d+-\d+-\d+T(?<time>\d+:\d+:\d+).+?\|(?<code>.+)\|.*?(?<sender>[A-Z][A-z']+? [A-z']+).*?\|(?<message>.+)\|[^]+/,
+}
